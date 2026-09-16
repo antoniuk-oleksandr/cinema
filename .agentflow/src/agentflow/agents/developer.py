ba0424d5780b0e0c754c2682293d74c64d645106
antoniuk-oleksandr <antonyk063@gmail.com>
2026-09-16 23:33:56 +0300

    chore(agents): replace Agentflow with team-friendly OpenClaw workflow

--- .agentflow/src/agentflow/agents/developer.py
+++
@@ -1,5 +0,0 @@
-from .base import Agent
-
-
-class DeveloperAgent(Agent):
-    pass
--- .agentflow/src/agentflow/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .issue import Issue
-from .result import AgentResult, AgentStatus, Route
-from .task import Task
-
-__all__ = ["AgentResult", "AgentStatus", "Issue", "Route", "Task"]
